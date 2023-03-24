5505deb57623a062d2f418b0abdcac0606b7e2dd
Muthuraaj <87850296+Muthuraaj@users.noreply.github.com>
2023-03-24 18:28:05 +0530

    Design completed

--- index.js
+++ index.js
@@ -4,10 +4,12 @@
 const button=document.getElementById('btn');
 const SideContent=document.getElementsByClassName('Sidebar')
 const Sidemenu=document.getElementById('sidebar')
-const mobSide=document.getElementsByClassName('sidemenus')
+const mobSide=document.getElementsByClassName('Mobclass')
 const mobBtn=document.getElementById('mobileButton')
 
+//getting windows width
 var windowWidth;
+//when a window screen change the function will call
 window.onresize=onload=()=>{
   windowWidth=this.innerWidth
   Sidebar(windowWidth)
@@ -19,9 +21,9 @@ function Sidebar(windowWidth){
     for(i=0;i<mobSide.length;i++){
       mobSide[i].style.display='none'
     }
-    for(i=0;i<SideContent.length;i++){
-      SideContent[i].style.display='inline';
-    }
+     for(i=0;i<SideContent.length;i++){
+       SideContent[i].style.display='block';
+     }
     
     mobBtn.addEventListener('click',mobilescreen)
 
@@ -33,7 +35,7 @@ function Sidebar(windowWidth){
       SideContent[i].style.display='none';
     }
     for(i=0;i<mobSide.length;i++){
-      mobSide[i].style.display='inline'
+      mobSide[i].style.display='block'
     }
     button.addEventListener('click',lapscreen)
   }
@@ -43,7 +45,7 @@ function mobilescreen(){
   if(Sidemenu.style.height==='5%'){
     Sidemenu.style.height='100%';
     for(i=0;i<mobSide.length;i++){
-      mobSide[i].style.display='inline'
+      mobSide[i].style.display='block'
     }
   }
   else{
@@ -57,13 +59,15 @@ function lapscreen(){
   if( Sidemenu.style.width==='5%'){
     Sidemenu.style.width='20%';
     for(i=0;i<SideContent.length;i++){
-      SideContent[i].style.display='inline';
+      SideContent[i].style.display='block';
+      
     }
   }
   else{
     Sidemenu.style.width='5%'
     for(i=0;i<SideContent.length;i++){
       SideContent[i].style.display='none';
+      
     }
   }
 }
